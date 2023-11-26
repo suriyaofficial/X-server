3173ec3993970f3ab6a434fe188d84778d6e260e
suriya <suriya.r@arbaan.com>
2023-11-26 15:26:45 +0530

    add

---
+++ firebaseconfig.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    apiKey: "AIzaSyDLqVtHV6CIJO7tekS7rvyrm1wOrS6UNaE",
+    authDomain: "chit-chat-sparrow.firebaseapp.com",
+    projectId: "chit-chat-sparrow",
+    storageBucket: "chit-chat-sparrow.appspot.com",
+    messagingSenderId: "981651993231",
+    appId: "1:981651993231:web:c230a297722ee8380176fa",
+    measurementId: "G-V9F7NKG8BE"
+};
+
+
---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const { initializeApp } = require("firebase/app");
+const { getFirestore } = require("@firebase/firestore");
+const { getDoc, doc, collection, setDoc } = require("firebase/firestore");
+const firebaseConfig = require("./firebaseconfig"); // Import the Firebase configuration
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+
+
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+const PORT = process.env.PORT || 3100;
+
+const init = async () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+
+    } catch (error) {
+        console.log("🚀 ~ file: index.js:13 ~ init ~ error:", error)
+
+    }
+}
+init()
+app.post("/users/", async (request, response) => {
+    const { username, name, password, gender, location } = request.body;
+    const hashedpassword = await bcrypt.hash(password, 10);
+    const docRef = doc(db, "Users", username);
+    const docSnap = await getDoc(docRef);
+    let resumeData = docSnap.data();
+    if (resumeData) {
+        response.status(400);
+        response.send({ result: "user already exist" })
+
+    } else {
+        const usersCollectionRef = collection(db, "Users");
+        const userDocRef = doc(usersCollectionRef, username); // Replace 'userId' with your desired ID
+        await setDoc(userDocRef, { username, name, hashedpassword, gender, location });
+        response.status(201);
+        response.send({ result: "user register sucessfully" })
+    }
+})
+
+app.post("/login/", async (request, response) => {
+    const { username, password } = request.body;
+    console.log("🚀 ~ file: index.js:54 ~ app.post ~ rquest.body:", request.body)
+    const docRef = doc(db, "Users", username);
+    const docSnap = await getDoc(docRef);
+    const getUser = docSnap.data();
+    console.log("🚀 ~ file: index.js:58 ~ app.post ~ getUser:", getUser)
+    if (getUser) {
+        console.log("user exist");
+        const passwordMatch = await bcrypt.compare(password, getUser.hashedpassword);
+        if (passwordMatch) {
+            const payload = { username: getUser.username };
+            const jwt_token = jwt.sign(payload, "token");
+
+            response.status(200)
+            response.send({ result: "loged in success", JWT: jwt_token })
+        } else {
+            response.status(401)
+            response.send({ result: "wrong password" })
+        }
+    } else {
+        response.status(404)
+        response.send({ result: "user not found" })
+    }
+
+
+});
