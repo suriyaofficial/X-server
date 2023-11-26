97b72b7674ef2bcfd05514a2e72bac3bfce2dc6c
suriya <suriya.r@arbaan.com>
2023-11-26 17:08:51 +0530

    re.a.a.a.a.a

--- index.js
+++ index.js
@@ -28,10 +28,46 @@ const port = 3100
 // }
 // init()
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('server running---ok ')
+const server = http.createServer(async (req, res) => {
+    if (req.method === 'GET' && req.url === '/weather') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Hi! Today\'s weather is cloudy.');
+    } else if (req.method === 'GET' && req.url === '/msg') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Hi! greeting');
+    }
+    else if (req.method === 'POST' && req.url === '/login/') {
+        const { username, password } = req.body;
+        console.log("🚀 ~ file: index.js:54 ~ app.post ~ rquest.body:", req.body)
+        const docRef = doc(db, "Users", username);
+        const docSnap = await getDoc(docRef);
+        const getUser = docSnap.data();
+        console.log("🚀 ~ file: index.js:58 ~ app.post ~ getUser:", getUser)
+        if (getUser) {
+            console.log("user exist");
+            const passwordMatch = await bcrypt.compare(password, getUser.hashedpassword);
+            if (passwordMatch) {
+                const payload = { username: getUser.username };
+                const jwt_token = jwt.sign(payload, "token");
+
+                res.status(200)
+                res.send({ result: "loged in success", JWT: jwt_token })
+            } else {
+                res.status(401)
+                res.send({ result: "wrong password" })
+            }
+        } else {
+            res.status(404)
+            res.send({ result: "user not found" })
+        }
+
+    } else {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/plain')
+        res.end('server running---ok ')
+    }
 })
 
 server.listen(port, () => {
