ffb8d21243651507d16ee2a7cd35acf0094cc3d0
suriyaboss1 <suriyaboss1@gmail.com>
2024-09-19 16:34:24 +0000

    .

---
+++ spend_firebaseconfig.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+    apiKey: "AIzaSyA5ALvrT1jy6AI60pKQzp1csJgX0Q7UY8I",
+    authDomain: "travel-spend-tracker.firebaseapp.com",
+    projectId: "travel-spend-tracker",
+    storageBucket: "travel-spend-tracker.appspot.com",
+    messagingSenderId: "368640826754",
+    appId: "1:368640826754:web:1c51e1d5794e6d40a7c39b"
+};
+
+
